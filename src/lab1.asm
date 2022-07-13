546fc74b7a3553652d794f0ea388a138ef50816d
mengshu22 <mscalander2020@outlook.com>
2022-07-13 20:50:15 +0800

    lab1.asm

--- src/lab1.asm
+++ src/lab1.asm
@@ -1,26 +1,16 @@
 .orig x3000
 
 ; initialize all register
-NOT R7, R0      ; R0 is ...1111...
-ADD R0, R0, R7
-NOT R0, R0
+AND R0, R0, #0      ; R0 is ...1111...
 ADD R0, R0, xF
 
-NOT R7, R1      ; R1 is the number to be tested
-ADD R1, R1, R7
-NOT R1, R1
+AND R1, R1, #0      ; R1 is the number to be tested
 
-NOT R7, R2      ; R2 is used to store tmp result
-ADD R2, R2, R7
-NOT R2, R2
+AND R2, R2, #0      ; R2 is used to store tmp result
 
-NOT R7, R3      ; use R3 to count loop time
-ADD R3, R3, R7
-NOT R3, R3
+AND R3, R3, #0      ; use R3 to count loop time
 
-NOT R7, R4      ; use R4 to store max loop time 12
-ADD R4, R4, R7
-NOT R4, R4
+AND R4, R4, #0      ; use R4 to store max loop time 12
 ADD R4, R4, #12
 
 
@@ -43,16 +33,15 @@ ADD R3, R3, #1
 NOT R2, R3
 ADD R2, R2, #1
 ADD R2, R2, R4
-BRn #3  ; stop
+BRn #4  ; stop
 
 BR #-12
 
+AND R2, R2, #0
 ADD R2, R2, #1
 HALT
 
-NOT R7, R2
-ADD R2, R2, R7
-NOT R2, R2
+AND R2, R2, #0
 HALT
 
 .end
